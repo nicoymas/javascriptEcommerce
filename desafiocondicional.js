a09528c0b592ab6b8dc82c54f0646445f044671d
nico viegas <nicolasmarinviegas@gmail.com>
2022-05-15 16:27:30 -0300

    commiting

---
+++ desafiocondicional.js
@@ -0,0 +1,17 @@
+let numero = prompt("ingrese un numero del 1 al 20: ")
+
+
+if (numero >= 1 && numero <= 20) {
+    alert ("el numeor es correcto")
+}else if (numero >=21){
+    
+    while (numero >=21){
+        if(numero >=21){
+            alert ("ingrese numero valido")
+            numero = prompt("ingrese un numero del 1 al 20: ")
+        }
+    
+
+    }
+} alert( "el numero" + numero + "es correcto")
+
---
+++ entregaforwhile.js
@@ -0,0 +1,15 @@
+let num = prompt('ingrese numero a multiplicar, caso contrario ingrese "0": ')
+
+while (num != 0){ 
+    
+   
+    let i
+        
+
+        for( i=1;i<=10;i++){
+            console.log(num + ' multiplicado por ' + i + ' es igual a '+ num*i)
+        }
+    num = prompt('ingrese numero a multiplicar, caso contrario ingrese "0": ')
+    
+
+}
